c6f7d05c374c49c8e4424ce60e50d6c92e2a031c
Bakyt Madi <bakyt.madi.work@gmail.com>
2021-03-16 21:53:29 +0300

    Add serializer factory

---
+++ oop_lab3/Classes/ProjectClasses/Factory/ItemSerializerFactory.cs
@@ -0,0 +1,18 @@
+﻿using oop_lab3.Classes.ItemClasses;
+using oop_lab3.Classes.SerializerAdapter;
+
+namespace oop_lab3.Classes.Factory
+{
+    public static class ItemSerializerFactory
+    {
+        public static ISerializer CreateSerializer(bool isXml)
+        {
+            if (isXml)
+            {
+                return new XmlSerializerAdapter(typeof(Item));
+            }
+
+            return new BinaryFormatterAdapter();
+        }
+    }
+}
